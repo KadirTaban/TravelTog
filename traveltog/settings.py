3ce28f4010c83e00b1df3e4132987c99092d6a4b
KadirTaban <kadirtaban08@gmail.com>
2022-08-30 14:41:24 +0300

    authorization are done

--- traveltog/settings.py
+++ traveltog/settings.py
@@ -41,12 +41,16 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',  # new
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'rest_framework',
+    'corsheaders',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  # new
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -81,13 +85,14 @@ DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'travelTOG',
-       'USER': os.environ['KOZALAK_UNAME'],
-       'PASSWORD': os.environ['KOZALAK_PASS'],
+       'USER': 'kozalak',
+       'PASSWORD':'0801shavsheti16..',
        'HOST': '127.0.0.1',
        'PORT': '5432',
    }
 }
 
+AUTH_USER_MODEL = "users.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -138,3 +143,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / 'uploads'
 MEDIA_URL = '/files/'
+
+CORS_ORIGIN_ALLOW_ALL=True
+CORS_ALLOW_CREDENTIALS=True
