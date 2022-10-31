68c3adbeca294ab03309b776b84363919f1a4cce
KadirTaban <kadirtaban08@gmail.com>
2022-10-31 08:31:17 +0300

    crashed all

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import MainFlowView, MyGalleryView
-
-
-urlpatterns=[
-    path('api/myGallery',MyGalleryView.as_view(), name="all-post"),
-    path('api/home',MainFlowView.as_view())
-] 
--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from .views import LogoutView, RegisterView,LoginView,UserView
-
-urlpatterns = [
-    path('register', RegisterView.as_view()),
-    path('login',LoginView.as_view()),
-    path('user',UserView.as_view()),
-    path('logout',LogoutView.as_view()),
-]
